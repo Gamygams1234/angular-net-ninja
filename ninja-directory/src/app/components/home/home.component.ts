549d0719a09c55f88de8ed3fa721b887a7680642
Gamaliel Burgos <gamyburgos@gmail.com>
2022-01-10 03:10:15 -0800

    Data Binding

--- ninja-directory/src/app/components/home/home.component.ts
+++ ninja-directory/src/app/components/home/home.component.ts
@@ -3,19 +3,19 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
   title: string = 'Task Tracker';
+  myString: string = 'I like Chicken';
+  myBoolean: boolean = true;
 
-  constructor() { }
+  constructor() {}
 
   // life cycle method
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
-  toggleAddTask(){
+  toggleAddTask() {
     console.log('Toggle');
   }
-
 }
